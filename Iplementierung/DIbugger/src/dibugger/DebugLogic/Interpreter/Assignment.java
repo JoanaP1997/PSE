c93756f168697451ad31610bc85fc4b5fd0183e2
Benedikt Wagner <udpto@student.kit.edu>
2018-01-10 21:23:27 +0100

    Fuer die Command Klassen Klassen angelegt. Es fehlt je die run()-Methode und Attribute

---
+++ Iplementierung/DIbugger/src/dibugger/DebugLogic/Interpreter/Assignment.java
@@ -0,0 +1,18 @@
+package dibugger.DebugLogic.Interpreter;
+
+import java.util.List;
+
+public class Assignment extends Command {
+
+    public Assignment(GenerationController controller) {
+	super(controller);
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public List<TraceState> run() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
---
+++ Iplementierung/DIbugger/src/dibugger/DebugLogic/Interpreter/WhileCommand.java
@@ -0,0 +1,18 @@
+package dibugger.DebugLogic.Interpreter;
+
+import java.util.List;
+
+public class WhileCommand extends Command {
+
+    public WhileCommand(GenerationController controller) {
+	super(controller);
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public List<TraceState> run() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
