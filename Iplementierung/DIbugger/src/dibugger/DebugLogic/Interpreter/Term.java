21bd811bf535d6370120382397da87a5813bb260
Benedikt Wagner <udpto@student.kit.edu>
2018-01-10 17:53:44 +0100

    Comparisons kommentiert

--- Iplementierung/DIbugger/src/dibugger/DebugLogic/Interpreter/Term.java
+++ Iplementierung/DIbugger/src/dibugger/DebugLogic/Interpreter/Term.java
@@ -2,13 +2,29 @@ package dibugger.DebugLogic.Interpreter;
 
 import java.util.List;
 /**
- * Represents a Term, which can be evaluated
+ * Represents a Term, which can be evaluated.
  * @author wagner
  *
  */
 public abstract class Term {
+    	/**
+    	 * Evaluates the Term replacing the variables with information from the given TraceStates
+    	 * @param states
+    	 * @return Value of the Term
+    	 */
 	public abstract TermValue evaluate(List<TraceState> states);
+	/**
+    	 * Evaluates the Term replacing the variables with information from the given Scope
+    	 * @param states
+    	 * @return Value of the Term
+    	 */
 	public abstract TermValue evaluate(Scope currentScope);
+	/**
+	 * Evaluates the Term replacing the variables with information from the given TraceStates
+	 * and returns a String
+	 * @param states
+	 * @return the value as a string
+	 */
 	public String evaluateToString(List<TraceState> states){
 		return this.evaluate(states).toString();
 	}
