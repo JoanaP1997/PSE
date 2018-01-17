c6ccbfed6abc5a2bd723b34cbe663f32b9e51fe3
Chiara <chiarastaudenmaier@outlook.de>
2018-01-17 10:34:02 +0100

    Code Felder sind scrollbar, Tab größe angepasst, Zeilennummerierung

--- Iplementierung/DIbugger/src/dibugger/UserInterface/ProgramPanel.java
+++ Iplementierung/DIbugger/src/dibugger/UserInterface/ProgramPanel.java
@@ -1,6 +1,10 @@
 package dibugger.UserInterface;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Element;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class ProgramPanel extends JPanel {
@@ -13,8 +17,9 @@ public class ProgramPanel extends JPanel {
     private JLabel EingabevariablenLabel;
     private JTextField EingabevariablenInput;
     private JPanel VariableWindow;
-    private JScrollPane jScrollPane1;
-    private JTextPane TextPaneCode;
+    private JScrollPane codeScrollPane;
+    private JTextArea codeArea;
+    private JTextArea lines;
 
     public ProgramPanel(int identifier) {
         id = identifier;
@@ -22,14 +27,13 @@ public class ProgramPanel extends JPanel {
     }
 
     private void initComponents() {
-        ProgramName = new javax.swing.JLabel();
-        Stepsize = new javax.swing.JLabel();
-        StepsizeInput = new javax.swing.JTextField();
-        EingabevariablenLabel = new javax.swing.JLabel();
-        EingabevariablenInput = new javax.swing.JTextField();
-        VariableWindow = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        TextPaneCode = new javax.swing.JTextPane();
+        ProgramName = new JLabel();
+        Stepsize = new JLabel();
+        StepsizeInput = new JTextField();
+        EingabevariablenLabel = new JLabel();
+        EingabevariablenInput = new JTextField();
+        VariableWindow = new JPanel();
+        codeScrollPane = new JScrollPane();
 
         ProgramName.setText("Programm: " + id);
 
@@ -44,53 +48,53 @@ public class ProgramPanel extends JPanel {
         EingabevariablenInput.addActionListener(this::EingabevariablenInputActionPerformed);
 
 
-        javax.swing.GroupLayout VariableWindowLayout = new javax.swing.GroupLayout(VariableWindow);
+        GroupLayout VariableWindowLayout = new GroupLayout(VariableWindow);
         VariableWindow.setLayout(VariableWindowLayout);
         VariableWindowLayout.setHorizontalGroup(
-                VariableWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                VariableWindowLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGap(0, 0, Short.MAX_VALUE)
         );
         VariableWindowLayout.setVerticalGroup(
-                VariableWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                VariableWindowLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGap(0, 171, Short.MAX_VALUE)
         );
 
-        jScrollPane1.setViewportView(TextPaneCode);
+        initCodeArea();
 
-        javax.swing.GroupLayout firstTextPanelLayout = new javax.swing.GroupLayout(this);
+        GroupLayout firstTextPanelLayout = new GroupLayout(this);
         setLayout(firstTextPanelLayout);
         firstTextPanelLayout.setHorizontalGroup(
-                firstTextPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                firstTextPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(firstTextPanelLayout.createSequentialGroup()
-                                .addGroup(firstTextPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(firstTextPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(firstTextPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addGroup(firstTextPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
                                                 .addGroup(firstTextPanelLayout.createSequentialGroup()
                                                         .addComponent(Stepsize)
-                                                        .addComponent(StepsizeInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                        .addComponent(StepsizeInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                                                 .addComponent(ProgramName)
                                                 .addComponent(EingabevariablenLabel)
                                                 .addComponent(EingabevariablenInput))
-                                        .addComponent(jScrollPane1)
-                                        .addComponent(VariableWindow, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                        .addComponent(codeScrollPane)
+                                        .addComponent(VariableWindow, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 )
         );
         firstTextPanelLayout.setVerticalGroup(
-                firstTextPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                firstTextPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(firstTextPanelLayout.createSequentialGroup()
                                 .addGap(30, 30, 30)
                                 .addComponent(ProgramName)
                                 .addGap(18, 18, 18)
-                                .addGroup(firstTextPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addGroup(firstTextPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                                         .addComponent(Stepsize)
-                                        .addComponent(StepsizeInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(StepsizeInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                                 .addGap(18, 18, 18)
                                 .addComponent(EingabevariablenLabel)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(EingabevariablenInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(EingabevariablenInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                                 .addGap(37, 37, 37)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 316, Short.MAX_VALUE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(VariableWindow, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(codeScrollPane, GroupLayout.DEFAULT_SIZE, 316, Short.MAX_VALUE)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(VariableWindow, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                                 .addGap(25, 25, 25))
         );
     }
@@ -101,6 +105,47 @@ public class ProgramPanel extends JPanel {
 
     private void EingabevariablenInputActionPerformed(ActionEvent evt) {
         // TODO add your handling code here:
+    }
+
+    private void initCodeArea() {
+        codeScrollPane = new JScrollPane();
+        lines = new JTextArea("1");
+        codeArea = new JTextArea();
+        lines.setBackground(Color.LIGHT_GRAY);
+        lines.setEditable(false);
+        codeArea.getDocument().addDocumentListener(new DocumentListener(){
+            String getText(){
+                int caretPosition = codeArea.getDocument().getLength();
+                Element root = codeArea.getDocument().getDefaultRootElement();
+                String text = "1" + System.getProperty("line.separator");
+                for(int i = 2; i < root.getElementIndex( caretPosition ) + 2; i++){
+                    text += i + System.getProperty("line.separator");
+                }
+                return text;
+            }
+            @Override
+            public void changedUpdate(DocumentEvent de) {
+                lines.setText(getText());
+            }
+
+            @Override
+            public void insertUpdate(DocumentEvent de) {
+                lines.setText(getText());
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent de) {
+                lines.setText(getText());
+            }
+
+        });
+        codeArea.setTabSize(3);
+
+        codeScrollPane.getViewport().add(codeArea);
+        codeScrollPane.setRowHeaderView(lines);
+        codeScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        codeScrollPane.setPreferredSize(new Dimension(400, 700));
+
     }
 }
  
