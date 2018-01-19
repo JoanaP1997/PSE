1b84e6a5bef4da8cd4fde5c96da33172a286420f
Benedikt Wagner <udpto@student.kit.edu>
2018-01-19 10:50:09 +0100

    DelreationAssignment hinzugefuegt

--- Iplementierung/DIbugger/src/dibugger/debuglogic/interpreter/Assignment.java
+++ Iplementierung/DIbugger/src/dibugger/debuglogic/interpreter/Assignment.java
@@ -9,41 +9,42 @@ import dibugger.debuglogic.exceptions.WrongTypeAssignmentException;
 
 /**
  * Command to assign a value to an already declared variable.s
+ * 
  * @author scheler, wagner
  *
  */
 public class Assignment extends Command {
-	private String identifier;
-	private Term term;
+  private String identifier;
+  private Term term;
 
-	public Assignment(GenerationController controller, int linenumber, String identifier, Term term) {
-		super(controller, linenumber);
-		this.identifier = identifier;
-		this.term = term;
-	}
+  public Assignment(GenerationController controller, int linenumber, String identifier, Term term) {
+    super(controller, linenumber);
+    this.identifier = identifier;
+    this.term = term;
+  }
 
-	@Override
-	public List<TraceState> run() throws DIbuggerLogicException {
-		Scope scope = this.controller.getCurrentScope();
-		TermValue value = this.term.evaluate(scope);
-		Type type = scope.getTypeOf(identifier);
-		
-		// check if variable exists
-		if (type == null) {
-			throw new VariableNotFoundException(this.linenumber);
-		}
-		
-		// check type
-		if (type != value.getType()) {
-			throw new WrongTypeAssignmentException(this.linenumber);
-		}
+  @Override
+  public List<TraceState> run() throws DIbuggerLogicException {
+    Scope scope = this.controller.getCurrentScope();
+    TermValue value = this.term.evaluate(scope);
+    Type type = scope.getTypeOf(identifier);
 
-		// set value
-		scope.setValueOf(this.identifier, value);
-		
-		List<TraceState> traceStateList = new ArrayList<TraceState>();
-		traceStateList.add(new TraceState(TraceStatePosition.NOTSPECIAL, this.linenumber, scope));
-		return traceStateList;
-	}
+    // check if variable exists
+    if (type == null) {
+      throw new VariableNotFoundException(this.linenumber);
+    }
+
+    // check type
+    if (type != value.getType()) {
+      throw new WrongTypeAssignmentException(this.linenumber);
+    }
+
+    // set value
+    scope.setValueOf(this.identifier, value);
+
+    List<TraceState> traceStateList = new ArrayList<TraceState>();
+    traceStateList.add(new TraceState(TraceStatePosition.NOTSPECIAL, this.linenumber, scope));
+    return traceStateList;
+  }
 
 }
