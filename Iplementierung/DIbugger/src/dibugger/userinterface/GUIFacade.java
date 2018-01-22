8a05d651bd779c597c4fb82ee0430aec8320100c
Chiara <chiarastaudenmaier@outlook.de>
2018-01-22 10:43:37 +0100

    GUIFacade an Google Checkstyle angepasst

--- Iplementierung/DIbugger/src/dibugger/userinterface/GUIFacade.java
+++ Iplementierung/DIbugger/src/dibugger/userinterface/GUIFacade.java
@@ -1,58 +1,67 @@
 package dibugger.userinterface;
 
+import dibugger.control.ControlFacade;
+import dibugger.userinterface.dibuggerpopups.DecisionPopUp;
+import dibugger.userinterface.dibuggerpopups.ErrorPopUp;
+
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
-import dibugger.userinterface.dibuggerpopups.DecisionPopUp;
-import dibugger.userinterface.dibuggerpopups.ErrorPopUp;
 
+/**
+ * Facade of the Dibuggers user interface.
+ */
 public class GUIFacade implements Observer {
-    private MainInterface mainInterface;
-   // private ControlFacade controlFacade;
+  private MainInterface mainInterface;
+  private ControlFacade controlFacade;
 
-    public GUIFacade(MainInterface mainInterface) {
-        this.mainInterface = mainInterface;
-        //controlFacade = new ControlFacade(this);
-    }
+  /**
+   * Constructs a new GUIFacade using the mainInterface.
+   * @param mainInterface main interface
+   */
+  public GUIFacade(MainInterface mainInterface) {
+    this.mainInterface = mainInterface;
+    controlFacade = new ControlFacade(this);
+  }
 
-    public void showProgramText(String programText, int id) {
-       //TODO
-    }
+  public void showProgramText(String programText, int id) {
+    //TODO
+  }
 
-    public void reset() {
-        //TODO
-    }
+  public void reset() {
+    //TODO
+  }
 
-    public void showInput(int program, List<String> vars) {
-        //TODO
-    }
-    
-    public List<String> getVariablesOfInspector(int programNumber) {
-        throw new UnsupportedOperationException();
-    }
-    
-    public void showVariables(int numberOfProgram, List<String> variables) {
-        //TODO
-    }
+  public void showInput(int program, List<String> vars) {
+    //TODO
+  }
 
-    @Override
-    public void update(Observable observable, Object o) {
-        //TODO
-    }
+  public List<String> getVariablesOfInspector(int programNumber) {
+    throw new UnsupportedOperationException();
+  }
 
-    public void showError(String s) {
-        new ErrorPopUp(s, mainInterface);
-    }
+  public void showVariables(int numberOfProgram, List<String> variables) {
+    //TODO
+  }
 
-    public void showWarning(String s) {
-        new DecisionPopUp(s, mainInterface);
-    }
-    //TODO: Rückgabewert
+  @Override
+  public void update(Observable observable, Object o) {
+    //TODO
+  }
 
-    public void changeLanguage() {
-        //TODO
-    }
+  public void showError(String s) {
+    new ErrorPopUp(s, mainInterface);
+  }
+
+  public void showWarning(String s) {
+    new DecisionPopUp(s, mainInterface);
+  }
+  //TODO: Rückgabewert
+
+  public void changeLanguage() {
+    //TODO
+  }
 
 
 }
