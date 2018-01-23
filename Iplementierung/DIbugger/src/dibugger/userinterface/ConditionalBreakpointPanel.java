9a907831e20d53b86e8a272ab8dd78d5caa151dc
Joana Plewnia <uhfpm@student.kit.edu>
2018-01-23 08:44:13 +0100

    Mergefehler in ConditionalBreakpointPanel behoben

--- Iplementierung/DIbugger/src/dibugger/userinterface/ConditionalBreakpointPanel.java
+++ Iplementierung/DIbugger/src/dibugger/userinterface/ConditionalBreakpointPanel.java
@@ -15,7 +15,6 @@ import java.util.HashMap;
 public class ConditionalBreakpointPanel extends ExpressionPanel {
 
   private static ConditionalBreakpointPanel singleton = null;
-<<<<<<< HEAD
 
   private JLabel title;
   private MainInterface mainInterface;
@@ -48,7 +47,7 @@ public class ConditionalBreakpointPanel extends ExpressionPanel {
 
   private void initComponents() {
 
-    idMap.put(0,0);
+    idMap.put(0, 0);
 
     panelType = "Conditional Breakpoints:";
     String[] columnTitles = {"Opt", panelType, "Auswertung"};
@@ -127,110 +126,17 @@ public class ConditionalBreakpointPanel extends ExpressionPanel {
     tableContainer.setPreferredSize(new Dimension(290, 250));
 
     this.add(tableContainer);
-=======
 
-  private JLabel title;
-  private MainInterface mainInterface;
-  private Object[][] dataEntries;
-  private ConditionalBreakpointPanel thisCBP = this;
-
-  private ConditionalBreakpointPanel(MainInterface mainInterface) {
-    this.mainInterface = mainInterface;
-    initComponents();
   }
 
-  public static ConditionalBreakpointPanel getConditionalBreakpointPanel(MainInterface mainInterface) {
-    if (singleton == null) {
-      singleton = new ConditionalBreakpointPanel(mainInterface);
-    }
-    return singleton;
-  }
 
-  public void update() {
-    //TODO
-  }
-
-  private void initComponents() {
-
-    panelType = "Conditional Breakpoints:";
-    String[] columnTitles = {"Opt", panelType, "Auswertung"};
-    dataEntries = new Object[1][3];
-    dataEntries[0][0] = " ";
-    dataEntries[0][1] = "hier könnte ihre Expression stehen";
-    dataEntries[0][2] = "ausgewertet";
-    DefaultTableModel tableModel = new DefaultTableModel(dataEntries, columnTitles) {
-      @Override
-      public boolean isCellEditable(int row, int column) {
-        return column == 1;
-      }
-    };
-    JTable table = new JTable(tableModel);
-    table.getColumnModel().getColumn(0).setPreferredWidth(5);
-    table.getColumnModel().getColumn(1).setPreferredWidth(150);
-    table.getColumnModel().getColumn(2).setPreferredWidth(5);
-    table.addMouseListener(new MouseListener() {
-      @Override
-      public void mouseClicked(MouseEvent mouseEvent) {
-        Point p = mouseEvent.getPoint();
-        if (table.columnAtPoint(p) == 0) {
-          new ExpressionChangePopUp(mainInterface, "ConditionalBreakpoint", 1, table, thisCBP);
-        } else if ((table.columnAtPoint(p) == 1)) {
-          //TODO: CB speichern
-        }
-        if (table.rowAtPoint(p) == dataEntries.length - 1 & table.columnAtPoint(p) == 1) {
-          Object[] newRow = {" ", "Fügen Sie hier Ihren neuen ConditionalBreakpoint ein ", ""};
-          tableModel.addRow(newRow);
-          ArrayList<Object[]> dataAsList = new ArrayList<Object[]>(dataEntries.length);
-          dataAsList.addAll(Arrays.asList(dataEntries));
-          dataAsList.add(newRow);
-          dataEntries = new Object[dataAsList.size()][];
-          for (int j = 0; j < dataAsList.size(); j++) {
-            dataEntries[j] = dataAsList.get(j);
-          }
-
-        }
-      }
-
-      @Override
-      public void mousePressed(MouseEvent mouseEvent) {
-
-      }
-
-      @Override
-      public void mouseReleased(MouseEvent mouseEvent) {
->>>>>>> fe56be852931053a579d3673e1ac97d051939dbe
-
-      }
-
-<<<<<<< HEAD
-  }
 
   public void saveScopes(int id, ArrayList<ScopeTuple> scopeTupels) {
     scopes.put(id, scopeTupels);
-=======
-      @Override
-      public void mouseEntered(MouseEvent mouseEvent) {
-
-      }
-
-      @Override
-      public void mouseExited(MouseEvent mouseEvent) {
-        //TODO: bei verlassen CB an Control geben
-      }
-    });
+    };
 
 
-    JScrollPane tableContainer = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-        JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-    tableContainer.setViewportView(table);
-    tableContainer.createVerticalScrollBar();
-    tableContainer.setPreferredSize(new Dimension(290, 250));
 
-    this.add(tableContainer);
-
-
->>>>>>> fe56be852931053a579d3673e1ac97d051939dbe
-  }
 
   public void deleteEntry(int id) {
     //TODO
