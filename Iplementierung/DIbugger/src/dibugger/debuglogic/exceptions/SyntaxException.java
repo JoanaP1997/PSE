b86d388257d9877d328ecdde71fb1ff13668f19d
Benedikt Wagner <udpto@student.kit.edu>
2018-01-25 14:18:35 +0100

    Ein bisschen Fehlerbehandlung, ein bisschen Freiheit...

--- Iplementierung/DIbugger/src/dibugger/debuglogic/exceptions/SyntaxException.java
+++ Iplementierung/DIbugger/src/dibugger/debuglogic/exceptions/SyntaxException.java
@@ -3,12 +3,22 @@ package dibugger.debuglogic.exceptions;
 public class SyntaxException extends DIbuggerLogicException {
 
   /**
-   * 
+   * Represents the recognition of a wrong syntactical input. 
+   * Wagner
    */
   private static final long serialVersionUID = 1L;
-
-  public SyntaxException() {
+  private String expected;
+ /**
+  * Constructor.
+ * @param expected the expected syntactical values.
+  */
+  public SyntaxException(String expected) {
     super(-1, "dl_exc_syntax");
+    this.expected = expected;
   }
-
+  @Override
+	public String getMessage() {
+		String message = super.getMessage();
+		return message + "\n\n" + this.expected;
+	}
 }
