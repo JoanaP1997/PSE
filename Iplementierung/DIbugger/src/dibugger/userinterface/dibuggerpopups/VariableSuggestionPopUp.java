db79bc80c0ed38af7658df481d3f39b3dc2c12f1
Joana Plewnia <uhfpm@student.kit.edu>
2018-01-25 14:24:29 +0100

    Vorschlagspopup für Variablen fertig. PSE

--- Iplementierung/DIbugger/src/dibugger/userinterface/dibuggerpopups/VariableSuggestionPopUp.java
+++ Iplementierung/DIbugger/src/dibugger/userinterface/dibuggerpopups/VariableSuggestionPopUp.java
@@ -18,14 +18,15 @@ public class VariableSuggestionPopUp extends DIbuggerPopUp {
 
   public VariableSuggestionPopUp (int message, MainInterface mainInterface) {
     this.mainInterface = mainInterface;
-    this.setSize(300,300);
+    this.setSize(300,310);
+    this.setResizable(false);
     this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 
-    //TODO: StepSize, Eingabevariablen, Expressions
+    //TODO: StepSize, Expressions
 
-    FlowLayout layout = new FlowLayout();
-    this.setLayout(layout);
-
+    GroupLayout layout = new GroupLayout(this);
+    getContentPane().setLayout(layout);
+    variableSuggestion();
 
     this.setModal(true);
     this.setVisible(true);
@@ -33,44 +34,145 @@ public class VariableSuggestionPopUp extends DIbuggerPopUp {
   }
 
   private void variableSuggestion() {
-    JLabel programLabel = new JLabel("Program: ");
-    JComboBox program = new JComboBox();
-    this.add(programLabel);
-    this.add(program);
-    ArrayList<String> programIds = mainInterface.getProgramIds();
-    for(String id: programIds) {
-      program.addItem(id);
-    }
-    this.add(new Label("Variable:"));
+    JLabel intervallStartLabel = new javax.swing.JLabel();
+    JTextField intervallStartField = new javax.swing.JTextField();
+    intervallStartField.setPreferredSize(new Dimension(100,20));
+    JLabel intervallEndLabel = new javax.swing.JLabel();
+    JTextField intervallEndField = new javax.swing.JTextField();
+    intervallEndField.setPreferredSize(new Dimension(100,20));
+    JLabel intervallLabel = new javax.swing.JLabel();
+    JLabel typeLabel = new javax.swing.JLabel();
+    JComboBox typeChooser = new javax.swing.JComboBox<>();
+    JButton okButton = new javax.swing.JButton();
+    JLabel suggestionLabel = new javax.swing.JLabel();
+    JLabel suggestedLabel = new javax.swing.JLabel();
 
-    JComboBox options = new JComboBox<>();
-    java.util.List<String> variable = mainInterface.getControlFacade().getDebugLogicFacade()
-        .getAllVariables((String)program.getSelectedItem());
-    for(String var: variable) {
-      options.addItem(var);
-    }
+    intervallStartLabel.setText("von:");
 
-    this.add(new Label("Vorschlag: "));
-    //TODO: Zwick in Hintern treten
-    String optionChoosed = options.getSelectedItem().toString();
-    //JTextField suggestionText = new JTextField(mainInterface.getControlFacade()
-        //.suggestInputValue(optionChoosed);
-    //this.add(suggestionText);
-    //suggestionText.setToolTipText("Vorgeschlagene Variable, kann noch manuell geändert werden. Bitte mit ok bestätigen.");
+    intervallStartField.setText("");
 
-    JButton ok = new JButton("Ok");
-    ok.addActionListener(new ActionListener() {
+    intervallEndLabel.setText("bis:");
+
+    intervallEndField.setText("");
+
+    intervallLabel.setText("Intervall");
+
+    typeLabel.setText("Type:");
+
+    typeChooser.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "int", "char", "long", "float", "double"
+        , "boolean" }));
+
+    okButton.setText("ok");
+
+    suggestionLabel.setText("Vorschlag:");
+
+    suggestedLabel.setText("Ok-Butto betätigen um Vorschlag zu erhalten");
+
+    javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+    getContentPane().setLayout(layout);
+    layout.setHorizontalGroup(
+        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(200, 200, 200)
+                        .addComponent(intervallLabel))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(29, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(intervallStartLabel)
+                                    .addComponent(typeLabel))
+                                .addGap(30, 30, 30)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING
+                                    , false)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(intervallStartField, javax.swing.GroupLayout.PREFERRED_SIZE
+                                            , javax.swing.GroupLayout.DEFAULT_SIZE
+                                            , javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(45, 45, 45)
+                                        .addComponent(intervallEndLabel)
+                                        .addGap(50, 50, 50)
+                                        .addComponent(intervallEndField, javax.swing.GroupLayout.PREFERRED_SIZE
+                                            , javax.swing.GroupLayout.DEFAULT_SIZE
+                                            , javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(typeChooser, javax.swing.GroupLayout.PREFERRED_SIZE
+                                        , 102, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(suggestionLabel))))
+                .addContainerGap(62, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(suggestedLabel)
+                    .addComponent(okButton))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+    );
+    layout.setVerticalGroup(
+        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(intervallLabel)
+                .addGap(35, 35, 35)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(intervallStartLabel)
+                    .addComponent(intervallStartField, javax.swing.GroupLayout.PREFERRED_SIZE
+                        , javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(intervallEndLabel)
+                    .addComponent(intervallEndField, javax.swing.GroupLayout.PREFERRED_SIZE
+                        , javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(50, 50, 50)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(typeLabel)
+                    .addComponent(typeChooser, javax.swing.GroupLayout.PREFERRED_SIZE
+                        , javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(suggestionLabel)
+                    .addComponent(suggestedLabel))
+                .addGap(35, 35, 35)
+                .addComponent(okButton)
+                .addContainerGap())
+    );
+
+    pack();
+
+    okButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        //TODO: Variable an Chiara geben
+        String type = typeChooser.getSelectedItem().toString();
+        int typeAsInt = getTypeFromString(type);
+        String startRange = intervallStartField.getText();
+        String endRange = intervallEndField.getText();
+        String suggestion = mainInterface.getControlFacade().suggestInputValue("x"
+            ,"(" + startRange + "," + endRange + ")", typeAsInt);
+        suggestedLabel.setText(type + " x = " + suggestion);
+        System.out.println(typeAsInt);
       }
     });
-
-    this.add(ok);
   }
 
   public static void main(String[] args) {
     VariableSuggestionPopUp p = new VariableSuggestionPopUp(1,new MainInterface());
+  }
+
+  private int getTypeFromString(String typeAsString) {
+    switch (typeAsString) {
+      case "char":
+        return 0;
+      case "int":
+        return 1;
+      case "long":
+        return 2;
+      case "float":
+        return 3;
+      case "double":
+        return 4;
+      case "boolean":
+        return 5;
+      default:
+        return -1;
+    }
   }
 
 
