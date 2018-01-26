bc95c8c447018a915b4f4770cd04891de72927f2
Joana Plewnia <uhfpm@student.kit.edu>
2018-01-26 10:43:26 +0100

    Javadocs hinzugefügt

--- Iplementierung/DIbugger/src/dibugger/userinterface/dibuggerpopups/ErrorPopUp.java
+++ Iplementierung/DIbugger/src/dibugger/userinterface/dibuggerpopups/ErrorPopUp.java
@@ -5,10 +5,19 @@ import dibugger.userinterface.MainInterface;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * PopUp that informs the user about an Error which can´t be handled by the logic itself. The user needs to change
+ * something to make the debugging process work
+ */
 public class ErrorPopUp extends DIbuggerPopUp {
 
     private MainInterface mainInterface;
 
+  /**
+   * constructor for an ErrorPopUp
+   * @param message String that is be shown to the user
+   * @param mainInterface MainInterface on which the PopUp is called
+   */
     public ErrorPopUp(String message, MainInterface mainInterface) {
         this.mainInterface = mainInterface;
         this.setSize(300, 100);
@@ -25,6 +34,7 @@ public class ErrorPopUp extends DIbuggerPopUp {
             CommandPanel.getCommandPanel(this.mainInterface).stopDebug();
             dispose();
         });
+
         this.add(ok, constraints);
 
         this.setVisible(true);
