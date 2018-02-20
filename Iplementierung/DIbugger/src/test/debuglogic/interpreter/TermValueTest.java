3b4b3be151e3decdfd96dbc096661d5a26c7483b
Benedikt Wagner <udpto@student.kit.edu>
2018-02-20 16:05:04 +0100

    TermValuetest Klasse erweitert.

--- Iplementierung/DIbugger/src/test/debuglogic/interpreter/TermValueTest.java
+++ Iplementierung/DIbugger/src/test/debuglogic/interpreter/TermValueTest.java
@@ -82,19 +82,39 @@ public class TermValueTest {
 
 	// tests the arithmetic operations
 	@Test
+	public void test_add_double_double_valid() throws DIbuggerLogicException {
+		DoubleValue v0 = new DoubleValue(1.6);
+		DoubleValue v1 = new DoubleValue(3.2);
+		assertTrue(Math.abs(((DoubleValue)v0.add(v1)).getValue()-4.8)<0.001);
+		assertTrue(Math.abs(((DoubleValue)v1.add(v1)).getValue()-6.4)<0.001);
+	}
+	@Test
 	public void test_add_float_float_valid() throws DIbuggerLogicException {
 		FloatValue v0 = new FloatValue(1.6f);
 		FloatValue v1 = new FloatValue(3.2f);
-		IntValue i0 = new IntValue(3);
 		assertEquals(v0.add(v1).toString(), "4.8");
-		assertEquals(v0.add(i0).toString(), "4.6");
-		assertEquals(((FloatValue) v0.sub(v1)).getValue(), -1.6f, 0.01f);// float
-																			// precison
-																			// problem
-																			// epsilon
-		assertEquals(((FloatValue) v0.mul(v1)).getValue(), 5.12f, 0.01f);
+		assertEquals(v1.add(v1).toString(), "6.4");
 	}
-
+	@Test
+	public void test_add_int_int_valid() throws DIbuggerLogicException {
+		IntValue i0 = new IntValue(3);
+		IntValue i1 = new IntValue(332);
+		assertEquals(i0.add(i1).toString(), "335");
+		assertEquals(i1.add(i1).toString(), "664");
+	}
+	@Test
+	public void test_add_long_long_valid() throws DIbuggerLogicException {
+		LongValue l0 = new LongValue(302392311);
+		LongValue l1 = new LongValue(332L);
+		assertEquals(l0.add(l1).toString(), String.valueOf(302392311+332L));
+		assertEquals(l1.add(l1).toString(), "664");
+	}
+	@Test
+	public void test_add_char_char_valid() throws DIbuggerLogicException {
+		CharValue c1 = new CharValue('#'); //0x23
+		CharValue c2 = new CharValue('%'); //0x25
+		assertEquals(c1.add(c2).toString(), "H"); //H = 0x48
+	}
 	// tests the clone methods
 	@Test
 	public void test_clone_float() {
