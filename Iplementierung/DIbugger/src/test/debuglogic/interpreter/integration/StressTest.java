141b0a6dc5c0a48415e5d203df28c4053104befc
Ulla Scheler <ulla.scheler@online.de>
2018-02-28 18:51:53 +0100

    neues Paket für Integrationstests erstellt

---
+++ Iplementierung/DIbugger/src/test/debuglogic/interpreter/integration/StressTest.java
@@ -0,0 +1,14 @@
+package test.debuglogic.interpreter.integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StressTest {
+
+    @Test
+    public void testAckermann() {
+        fail("Not yet implemented");
+    }
+
+}
